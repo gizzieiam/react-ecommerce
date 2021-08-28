8a7a101b2cdbee9fa1243da2d10cf7d03f5e29cd
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-08-27 17:07:39 -0700

    display cart items

--- src/Components/cart/ViewCart.js
+++ src/Components/cart/ViewCart.js
@@ -5,18 +5,31 @@ import Products from './../../Data/Products';
 function ViewCart(props){
     let show = []
     for (let i = 0; i < localStorage.length; i++) {
-        const element = localStorage.getItem(localStorage.key(i));
-        let item = regex(element, 'name')
-        let amount = regex(element, 'amount')
-        for (let j = 0; j < Products.length; j++) {
-            const elements = Products[j];
-            
-            // console.log(elements);
+        const kart = localStorage.getItem(localStorage.key(i));
+        let item = regex(kart, 'name')
+        let amount = regex(kart, 'amount')
+
+        for (const name of Products) {
+            let curr = name.productName
+            if(item === curr){
+                show.push(
+                    <li>
+                        <h1>
+                            {item}
+                        </h1>
+
+                        <p>Qt. {amount}</p>
+
+                    </li>
+                )
+            }
         }
+
 
     }
     return(
         <>
+            {show}
         </>
     )
 }
