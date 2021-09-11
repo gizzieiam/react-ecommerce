677326dc38515cde55070558f68b2513eb5ac9b3
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-09-11 14:07:55 -0700

    refactor code

---
+++ src/Components/checkout/backend/index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express()
+
+app.use(
+    express.urlencoded(
+        {extended:false}
+    )
+)
+app.use(express.json())
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+const port = process.env.PORT || 3000
+
+app.listen(port, function(err){
+    if(err) throw err
+    console.log("app running on port", ${port});
+})
